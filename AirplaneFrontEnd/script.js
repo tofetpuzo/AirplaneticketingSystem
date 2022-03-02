ace8a1a749ca924e27df78faa9afe3729e05a2d8
tofetpuzo <topetemiola09@gmail.com>
2022-03-02 20:56:03 +0000

    Developing the front end

--- AirplaneFrontEnd/script.js
+++ AirplaneFrontEnd/script.js
@@ -1,4 +1,16 @@
-function btnClicked() {
-    document.getElementById('flight').style.display ="block";
-  
-}
+const btn = document.getElementById('btn-flight');
+
+btn.addEventListener('click' , () =>{
+    const form = document.getElementById('flight-form');
+
+    if(form.style.display === 'none'){
+        // this shows the form
+        form.style.display = 'block';
+    }else{
+        // this hides the form
+        form.style.display = 'none';
+    }
+
+});
+
+
