d8f28ff3903d5bfb0e9d4443b387a27dc95c65f5
tofetpuzo <topetemiola09@gmail.com>
2022-05-15 15:31:32 +0100

    working on the backend for the server

--- Airplane-Design/Airplane-Back-end/server.js
+++ Airplane-Design/Airplane-Back-end/server.js
@@ -1,6 +1,35 @@
 var express = require('express');
+const fs = require('fs')
+const http = require('http')
+const port = 4000
 var app = express();
 
 app.get("/Airplane-Front-End/flightbooking.html", async (req, res) => {
 
 });
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', function(error , data){
+        if (error){
+            res.write(404)
+            res.write('Error: File Not found')
+        }else{
+            
+        }
+
+    })
+    if (req.method === 'GET'){
+        
+
+    }
+
+});
+
+server.listen(port, function(error){
+    if(error) console.log('Something went wrong', error)
+    else{
+        console.log('Server is listening on port' + port)
+    }  
+
+})
---
+++ Airplane-Design/Airplane-Front-End/main.py
@@ -0,0 +1,22 @@
+# arr = []
+
+# list_item = int(input('Enter items in a list: '))
+# while(len(s) < 3):
+#     s = arr.append(list_item)
+
+#    print(list_item, end=' ')
+#    s = arr.append(list_item)
+
+
+# def up_array(lst):
+#     for item in lst:
+#         print(arr)
+
+# up_array(arr)
+
+
+
+
+#         total.append(second_item)
+#         dic[key] = sum(total)
+# print(dic)
