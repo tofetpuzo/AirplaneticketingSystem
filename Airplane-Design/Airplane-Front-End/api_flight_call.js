68fd90a05403b0fb36ea96f68a734d9a3ed95cd6
tofetpuzo <topetemiola09@gmail.com>
2022-05-16 21:55:06 +0100

    working on the backend for the server

--- Airplane-Design/Airplane-Front-End/api_flight_call.js
+++ Airplane-Design/Airplane-Front-End/api_flight_call.js
@@ -1,13 +1,17 @@
 const request = require('request');
+const searchBy = `${}`
 
 const options = {
     method: 'GET',
+
     url: 'https://aerodatabox.p.rapidapi.com/flights/number/EW789/2022-05-23',
+    // url: 'https://aerodatabox.p.rapidapi.com/flights/number/EW789/2022-05-23',
     headers: {
         'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com',
-        'X-RapidAPI-Key': '985e972acfmshc856f747e805ad5p1bbb5cjsn558a69fc7db0',
+        'X-RapidAPI-Key': ''
         useQueryString: true
     }
+    https://aerodatabox.p.rapidapi.com/flights/{searchBy}/{searchParam}/dates/{fromLocal}/{toLocal}
 };
 
 request(options, function (error, response, body) {
